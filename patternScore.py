58f60bb842044dacfee372a23c2aeaf1b7f3196f
flounderK <flounderK@github.com>
2018-02-17 16:38:39 -0500

    Added some docstrings and made sure to avoid a divide by zero error in document's score function

--- patternScore.py
+++ patternScore.py
@@ -12,6 +12,8 @@ class Scored_Pattern():
 class Score_Legend(set):
     """List of Scored_Pattern objects"""
     def find_patterns_by_score(self, score):
+        """Returns a list of all of the Scored_Pattern objects
+        that have a score attribute that matches the provided number"""
         return_list = list()
         for i in self:
             if i.score == score:
@@ -19,16 +21,22 @@ class Score_Legend(set):
         return return_list
 
     def pattern_exists(self, pattern):
+        """Check if there is a pattern in this instance that matches the
+        provided string"""
         for i in self:
             if i.pattern == pattern:
                 return True
         return False
 
     def add(self, Scored_Pattern):
+        """Works just like add for the 'set' class, but doesn't allow multiple
+        of the same pattern"""
         if not self.pattern_exists(Scored_Pattern.pattern):
             super(Score_Legend, self).add(Scored_Pattern)
 
 class Document():
+    """This class just contains some text, a Score_Legend to score it by,
+    and and optional title and author"""
     def __init__(self, legend, text, author=None, title=None):
         self.text = text
         self.author = author
@@ -37,6 +45,8 @@ class Document():
         self.score_total = self.score()
 
     def score(self):
+        """Scores the provided document based on the number of matches with the
+        patterns in the legend's regex"""
         total = 0
         match_total = 0
         for scored_pattern in self.legend:
@@ -44,5 +54,6 @@ class Document():
             for match in matches:
                 total = total + int(scored_pattern.score)
                 match_total = match_total + 1
-        self.score_total = total /match_total
+        if not (total == 0 or match_total == 0):
+             self.score_total = total /match_total
         return self.score_total
