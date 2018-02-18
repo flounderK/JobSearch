89909b4635e55f43a97b584d533fcb409e8acd42
flounderK <clifwolfe1@gmail.com>
2018-02-18 01:01:38 -0500

    Updated PAL
    Added loop to find the titel, description, and organization name of each posting.

--- PAL.py
+++ PAL.py
@@ -4,7 +4,8 @@ Created on Tue Feb 13 13:04:24 2018
 
 @author: Clif
 """
-
+#TODO: make the text finding/parsing stuff into a function, get rid of nested
+#ifs in that function. Hint: include panels assignment in function
 import time
 from selenium import webdriver
 from selenium.webdriver import ActionChains
@@ -12,11 +13,11 @@ from selenium.webdriver.support import ui
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import psutil
+from urllib.parse import urljoin
+from re import sub, compile
 
 def page_is_loaded(driver):
     return driver.find_element_by_tag_name("body") != None
-
-
 
 
 username = ""
@@ -29,11 +30,13 @@ actions = ActionChains(driver)
 
 driver.get("https://webapps2.uc.edu/elce/Login")
 
-username_field = driver.find_element_by_id("username")
-username_field.send_keys(username)
-password_field = driver.find_element_by_id("password")
-password_field.send_keys(password)
-password_field.send_keys(Keys.RETURN)
+#wait
+
+#username_field = driver.find_element_by_id("username")
+#username_field.send_keys(username)
+#password_field = driver.find_element_by_id("password")
+#password_field.send_keys(password)
+#password_field.send_keys(Keys.RETURN)
 
 
 driver.find_element_by_css_selector(".col-md-2 > a:nth-child(7)").click()
@@ -49,15 +52,32 @@ soup = BeautifulSoup(content, "lxml")
 table = soup.find("table", id="search-results-table")
 rows = table.find_all("tr", role="row")
 
-
-#select the rank button
-# for i in range(1, (len(rows)-1)):
-    #open in new tab
-    # posting = driver.find_element_by_css_selector(("#search-results-table "+
-    #                                                "> tbody "+
-    #                                                "> tr:nth-child("+str(i)+
-    #                                                ") "+
-    #                                                "> td:nth-child(4) > a"))
-    # actions.key_down(Keys.CONTROL).click(posting).key_up(Keys.CONTROL).perform()
-
-    #switch to new tab
+sub_re = compile("\\n|\\xa0")
+for i in range(1, (len(rows)-1)):
+    href = rows[i].find("a")['href']
+    url = urljoin(driver.current_url, href)
+    driver.get(url)
+    #wait
+    soup = BeautifulSoup(driver.page_source, "lxml")
+    panels = soup.find_all("div", class_="panel panel-default")
+   
+    for k in panels:
+        #remove \n and \xa0 with sub
+        #Position Name
+        tag = k.find("h2")
+        if tag is not None:
+            if sub(sub_re, "",tag.text) == 'Rank Position':
+                title = sub(sub_re,"",k.find("div", attrs={"class":"row"}).text)
+        #Position Description
+        tag = k.find("div", attrs={"class":"pal-label"})
+        if tag is not None:
+            if sub(sub_re, "",tag.text) == "Position Description":
+                text = sub(sub_re, "",k.find("div", attrs={"class":"pal-content"}).text)
+        #Organization Name
+        tag = k.find("div", attrs={"class":"pal-label"})
+        if tag is not None:
+            if sub(sub_re, "",tag.text) == "Organization Name":
+                author = sub(sub_re, "",k.find("div", attrs={"class":"pal-content"}).text)
+    
+    #grade text, author bias, ect.
+    
