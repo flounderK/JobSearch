8feb84dc12bfd23f634f96334bd3aead19582830
flounderK <clifwolfe1@gmail.com>
2018-02-19 14:49:59 -0500

    consolodated created tables
    consolodated tables into one big table.

--- createDataBase.py
+++ createDataBase.py
@@ -7,22 +7,20 @@ Created on Sun Feb 18 17:46:04 2018
 
 import sqlite3 
 from os.path import exists
-from sys import exit
 
-dbName = 'JobPostings.db'
-if exists(dbName):
-    exit()
+def create_db():
+    dbName = 'JobPostings.db'
+    if exists(dbName):
+        return    
+    conn = sqlite3.connect(dbName)
+    c = conn.cursor()
+    c.execute('''CREATE TABLE postings
+                  (postingID int PRIMARY KEY, 
+                  postingText text, 
+                  title text, 
+                  author text)''')
     
-conn = sqlite3.connect(dbName)
-c = conn.cursor()
-c.execute('''CREATE TABLE postings
-              (postingID int PRIMARY KEY, 
-              textID int, 
-              title text, 
-              authorID int)''')
-c.execute('''CREATE TABLE authors
-              (authorID int PRIMARY KEY, authorName text)''')
-c.execute('''CREATE TABLE postingTexts
-              (textID int PRIMARY KEY, postingText text)''')
-
-conn.close()
+    conn.close()
+    
+def insert_into_db():
+    pass
